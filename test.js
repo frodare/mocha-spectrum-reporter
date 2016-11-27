db76ea9f2c54898c96baa1b1c5494519e9502521
Charles Howard <cdhoward@add123.com>
2016-11-26 23:17:12 -0500

    - Adding a test file - Updated Travis CI

---
+++ test.js
@@ -0,0 +1,7 @@
+var spectrum = require('./index.js');
+var expect = require('expect');
+describe('Spectrum', function () {
+	it('should exists', function() {
+		expect(spectrum).toExist();
+	});
+});
